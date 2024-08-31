e9792bd2ce9d99ec6b546ee94806703e26183bc7
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-08-31 09:04:35 -0300

    create folder provas and create file main.py

---
+++ Python/provas/prova1_listas_e_tuplas/main.py
@@ -0,0 +1,7 @@
+frutas = ["maca","banana","mamao","melancia","pera"]
+print(frutas)# exibe o conteudo da lista
+print(type(frutas))# exibe o tipo deste dado
+
+nova_frutas = tuple(frutas)
+print(nova_frutas)
+print(type(nova_frutas))
